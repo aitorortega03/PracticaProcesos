cb322c7f7f9531f2e71ff6a90ecb51d3ecf04374
israel <is>
2020-12-12 11:01:02 +0100

    Responsive

--- app/src/main/java/com/example/infocovid/SplashScreen.java
+++ app/src/main/java/com/example/infocovid/SplashScreen.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 
+import com.example.infocovid.View.MainActivity;
+
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -18,7 +20,7 @@ import java.util.TimerTask;
         TimerTask tarea = new TimerTask() {
             @Override
             public void run() {
-                Intent intent = new Intent(SplashScreen.this,MainActivity.class);
+                Intent intent = new Intent(SplashScreen.this, MainActivity.class);
                 startActivity(intent);
                 finish();
             }
