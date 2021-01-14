ea49f26fd5b259c97384c494cbca5e00aefc383d
Aitor <aitor.o.p.00@gmail.com>
2021-01-14 13:29:44 +0100

    Perfil y login unidos con exito

--- app/src/main/java/com/example/infocovid/View/profileFragment.java
+++ app/src/main/java/com/example/infocovid/View/profileFragment.java
@@ -7,6 +7,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.example.infocovid.R;
 
@@ -47,7 +48,10 @@ public class profileFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        View vista = inflater.inflate(R.layout.fragment_profile, container, false);
+        TextView nick = vista.findViewById(R.id.tvNickname);
+        nick.setText(MainActivity.nombreUsuario);
+        return vista;
+
     }
 }
